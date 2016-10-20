f45fd13e20a2b0ccf3ffde0119672ec9edfc73cd
Carson McLean <camclean@ualberta.ca>
2016-10-20 14:18:29 -0600

    Hello.py become RESTful.

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Resource, Api
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, "/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
